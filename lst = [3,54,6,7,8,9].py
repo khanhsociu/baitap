3b25ab086e81d86c1d29a39b8e0a8935e5ce7004
khanhsociu <84558770+khanhsociu@users.noreply.github.com>
2021-09-18 11:03:24 +0700

    Add files via upload

---
+++ lst = [3,54,6,7,8,9].py
@@ -0,0 +1,7 @@
+lst = [3, 54, 6, 7, 8 ,9]
+# Sắp xếp các phần tử trong list theo thứ tự tăng dần
+lst.sort()
+print(lst)
+# Sắp xếp các phần tử trong list theo thứ tự giảm dần
+lst.sort(reverse=True)
+print(lst)
